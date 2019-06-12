349094c0f3826e232c2a3e804719ef47872b22b4
ANasypana <alla.nasypana@gmail.com>
2019-06-13 00:19:06 +0300

    first part of task

---
+++ lesson_3_1/lesson_3_2.py
@@ -0,0 +1,12 @@
+# Найти сумму элементов матрицы
+
+def sum_elements(l):
+    sum = 0
+    for row in l:
+        for elem in row:
+            sum += int(elem)
+    return sum
+
+if __name__=='__main__':
+    array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(sum_elements(array))
