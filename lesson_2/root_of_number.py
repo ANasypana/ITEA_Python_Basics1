5e3b9bdbeff317b091be9debb7f94c3fedd6e49c
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-15 04:56:29 +0300

    Update and rename lesson _2_3.py to root_of_number.py

--- lesson_2/root_of_number.py
+++ lesson_2/root_of_number.py
@@ -8,10 +8,13 @@ if __name__=='__main__':
     number = int(input())
 
     if math.sqrt(number) > 10:
-        while math.sqrt(number) > 10:
+        
+        while math.sqrt(number) > 10:            
             print(math.sqrt(number))
+            
             print('Enter number:')
             number = int(input())
+            
         print('Square root of number <10')
     else:
-        print('Square root of number <10')
+        print('Square root of number <10') 
