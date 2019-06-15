b1a9d8b2658e886f2eb6f19bd8906d169034ab5d
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-15 05:48:39 +0300

    Update check_for_ prime_number.py

--- homework/check_for_ prime_number.py
+++ homework/check_for_ prime_number.py
@@ -3,14 +3,16 @@
 def prime_num(a):
     p = 1
     root = int(a**0.5)
+    
     for i in range(2, (root + 1)):
         p = p * (a % i)
-       
+     
     if p == 0:
         return print('Composite number')
+    
     return print('Prime number')
 
 
 if __name__=='__main__':
     a = 8
-    prime_num(a) 
+    prime_num(a)  
