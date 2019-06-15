6099cf8d1042265aa17adbffc723e3e3fdebc78f
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-15 05:23:29 +0300

    Update and rename lesson _2_9.py to fibonacci_numbers.py

--- lesson_2/fibonacci_numbers.py
+++ lesson_2/fibonacci_numbers.py
@@ -4,10 +4,10 @@ if __name__=='__main__':
 
     print('Enter n:')
     number = int(input())
+    
     list = [1, 1]
-    for i in range(2,number):
+    
+    for i in range(2, number):
         list.append(list[i-2]+list[i-1])
 
-    print('Fibonacci numbers: ', list)
-
-
+    print('Fibonacci numbers: ', list) 
