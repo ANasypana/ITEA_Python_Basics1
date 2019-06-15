fc01b2a4ca08b4e20123c7905425366d4c21f39c
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-15 05:00:22 +0300

    Update and rename lesson _2_3_1.py to mean_array.py

--- lesson_2/mean_array.py
+++ lesson_2/mean_array.py
@@ -1,17 +1,19 @@
 # Написать функцию, которая вычисляет среднее арифметическое элементов массива, переданного ей в качестве аргумента
 
-def avrg_array(l):
+def mean_array(l):
     sum = 0
     size = len(l)
     for i in l:
         sum = sum + int(i)
     return float(sum/size)
 
+
 if __name__=='__main__':
 
     print('Enter the list of numbers (through ","):')
     a=str(input())
+    
     array = a.split(",")
+    
     print(array)
-    print('Result of avrg_array:', avrg_array(array))
-
+    print('Result of mean_array:', mean_array(array)) 
