77d076eac45cf0866795a0e1a83952c6cb6bab9b
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-15 04:41:11 +0300

    Update and rename lesson_1_4.py to sort_array.py

--- lesson_1/sort_array.py
+++ lesson_1/sort_array.py
@@ -1,11 +1,14 @@
 # Напишите программу, которая сортирует заданный список из чисел
+
 import string
+
 if __name__=='__main__':
+    
     temporary = ''
     array = []
     # print('Enter the list of numbers (through ','):')
     # a = str(input())
-    a='3453, 6,89,999,7'
+    a ='3453, 6,89,999,7'
     for i in a:
         if i != ',':
             temporary = temporary+i
@@ -20,23 +23,13 @@ if __name__=='__main__':
 
     for i in range(lenth_array):
        index_min = i
+    
        for j in range(i+1, lenth_array):
           if sort_array[j] < sort_array[index_min]:
               index_min = j
+                
        tmp = sort_array[index_min]
        sort_array[index_min] = sort_array[i]
        sort_array[i] = tmp
-    print(sort_array)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    print(sort_array) 
