eee10317ad2a5a0b76ff1f4a34b6ad14f78eef6b
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-15 05:41:44 +0300

    Update and rename lesson_2_4_3.py to reverse_digits.py

--- lesson_2/reverse_digits.py
+++ lesson_2/reverse_digits.py
@@ -1,6 +1,7 @@
 # Дано натуральное число N. Выведите все его цифры по одной, в обратном порядке, разделяя их пробелами или новыми строками
 
 def print_digits(n):
+    
     if n < 10:
         print(n)
     else:
@@ -11,5 +12,6 @@ def print_digits(n):
         return print_digits(a)
 
 if __name__=='__main__':
+    
     n = 246
-    print_digits(n)
+    print_digits(n) 
