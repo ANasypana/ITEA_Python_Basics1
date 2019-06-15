66cc54bcf90ca1c1123db37a57b955ea48b33020
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-15 05:17:02 +0300

    Update and rename lesson _2_5.py to sum_of_numbers.py

--- lesson_2/sum_of_numbers.py
+++ lesson_2/sum_of_numbers.py
@@ -4,11 +4,10 @@ if __name__=='__main__':
 
     print('Enter list of numbers (through ", "):')
     list_of_word = str(input())
-    l=list_of_word.split(",")
+    
+    l = list_of_word.split(",")
     Sum = 0
     for i in l:
         Sum =Sum+int(i)
-    print('Sum: ',Sum)
-
-
-
+    
+    print('Sum: ',Sum) 
