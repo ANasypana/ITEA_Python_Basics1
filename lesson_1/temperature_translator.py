2fc5d242b72467ddbd7bc02c0f66cd84c6421175
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-15 04:46:58 +0300

    Update and rename lesson_1_6.py to temperature_translator.py

--- lesson_1/temperature_translator.py
+++ lesson_1/temperature_translator.py
@@ -4,8 +4,9 @@ if __name__=='__main__':
 
     print('Enter temperature values in Kelvin:')
     kelvin = float(input())
+    
     if kelvin < 0:
         print('It is not correct')
     else:
         celsius = kelvin-273.15
-        print(round(celsius),' C')
+        print(round(celsius),' C') 
