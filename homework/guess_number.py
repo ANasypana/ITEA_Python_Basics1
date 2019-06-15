54e71768a3fed5d287c3aa4ea9a422dcd41eb250
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-06-15 04:27:20 +0300

    Update and rename homework 2_7.py to guess_number.py

--- homework/guess_number.py
+++ homework/guess_number.py
@@ -6,17 +6,15 @@ if __name__=='__main__':
 
     a = random.randint(0,10)
     x = -1
+    
     while x != a:
         print('Enter number from o to 10:')
         x = int(input())
         if x == a:
             print('Success!')
+            
         elif x < a:
             print('The number is larger')
 
         else:
-            print('The number is less')
-
-
-
-
+            print('The number is less') 
