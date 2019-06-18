b7ca69c787e7613f1971bf9313bc8a0bb74892c6
ANasypana <alla.nasypana@gmail.com>
2019-06-19 00:19:35 +0300

    tasks from lesson 1

---
+++ lesson_1/lesson_1_7.py
@@ -0,0 +1,19 @@
+# Напишите программу, которая преобразует строку, в которой записаны слова через “,” в список из этих слов
+
+import string
+
+
+if __name__=='__main__':
+    line = 'ooojuoi, tyiu, ll, , k'
+    temporary = ''
+    array = []
+
+    for i in line:
+        if i != ',':
+            temporary = temporary+i
+        else:
+            array.append(temporary.strip(' '))
+            temporary = ''
+    array.append(temporary.strip(' '))
+
+    print(array)
