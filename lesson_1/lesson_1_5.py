28a487e1ab25993b1120a92f758059f57cf1325b
ANasypana <alla.nasypana@gmail.com>
2019-06-25 18:19:29 +0300

    add correction in the task 5

--- lesson_1/lesson_1_5.py
+++ lesson_1/lesson_1_5.py
@@ -1,20 +1,19 @@
 # Напишите программу, которая считает сумму последних цифр двух введенных пользователем чисел
 
-if __name__ == '__main__':
-    first_number = input('Enter first number:\n')
-    first_number = str(first_number)
-    while not first_number.isdigit():
-        first_number = input('Error! Try again. You must enter only number:\n')
-        first_number = str(first_number)
 
-    second_number = input('Enter second number:\n')
-    second_number = str(second_number)
-    while not second_number.isdigit():
-        second_number = input('Error! Try again. You must enter only number:\n')
-        second_number = str(first_number)
+def enter_number(number):
+    num = input(f'Enter {number}:\n')
+    while not num.isdigit():
+        num = input('Error! Try again. You must enter only number:\n')
+    return num
+
+
+if __name__ == '__main__':
+    first_number = enter_number('first number')
+    second_number = enter_number('second number')
 
     first_digit = first_number[-1]
     second_digit = second_number[-1]
-    
+
     result = int(first_digit) + int(second_digit)
-    print('Sum of the last digits: ', result)
+    print(f'Sum of last digits - {result}')
