47719714a24b4263366cb52e1b3927ec5ec4c096
ANasypana <51461431+ANasypana@users.noreply.github.com>
2019-07-01 21:35:50 +0300

    Update copy_array.py

--- lesson_3_2/copy_array.py
+++ lesson_3_2/copy_array.py
@@ -3,7 +3,7 @@
 import copy
 
 
-def copy_array(l):
+def copy_function(l):
     result_copy = copy.copy(l)
     return result_copy
 
@@ -12,11 +12,11 @@ if __name__ == '__main__':
     array = [w * 2 for w in 'python']
     dictionary = {a: a ** 2 for a in range(1, 6)}
 
-    print(copy_array(array))
-    print(copy_array(dictionary))
+    print(copy_function(array))
+    print(copy_function(dictionary))
 
     dictionary[7] = 7 ** 2
     array.append('java')
 
-    print(copy_array(array))
-    print(copy_array(dictionary))
+    print(copy_function(array))
+    print(copy_function(dictionary))
